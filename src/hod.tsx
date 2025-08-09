d3ee7d6e9f65b938b48fb5cddd982bda7c39680c
emmanuvalmb <emmanuvalbaiju512@gmail.com>
2025-08-09 07:20:06 +0530

    pushed

---
+++ src/hod.tsx
@@ -0,0 +1,29 @@
+import { Routes, Route } from 'react-router-dom';
+
+import Sidebar from './components/Dashboard/sidebar';
+import Api from './components/Dashboard/U-Events';
+import RApi from './components/Dashboard/R-Activity';
+import Form from './NewEvent'; // or './components/form' if it's inside components
+
+function App() {
+    return (
+        <div style={{ display: 'flex' }}>
+            <Sidebar /> {/* Sidebar always visible */}
+
+            <div style={{ flex: 1, padding: '20px' }}>
+                <Routes>
+                    {/* Home/Dashboard */}
+                    <Route path="/hod" element={
+                        <>
+                        <Api />
+                        <RApi />
+                        </>
+                      } />
+                    <Route path='/eventcreate' element={<Form />} /></Routes>
+                    
+            </div>
+        </div>
+    );
+}
+
+export default App;
