3784bac0eaec29c48fc0a9320607c8cd19ea2626
Anjo Samson <anjosamson05@gmail.com>
2025-10-26 17:41:27 +0530

    deployyy

--- src/App.tsx
+++ src/App.tsx
@@ -27,11 +27,7 @@ const App: React.FC = () => {
         {/* Home page route */}
         <Route path="/" element={
           <>
-          
-            <Header />
-            <Hero />
-            <News />
-            <Footer />
+          <Login />
           </>
         } />
         
